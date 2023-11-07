35c4ce31fe7daa552cfd254992b55d8210eca74d
Konrad Pagacz <konrad.pagacz@gmail.com>
2023-11-07 21:19:08 +0100

    refactor: added py.typed
    * Apparently, if we want mimy and other modules to detect
      that the library uses typing, we should add an empty file - py.typed
      to the project.
    * This is according to [PEP-561](https://peps.python.org/pep-0561/#packaging-type-information).
    * I also reran the black formatter on the files, but I might have set it up wrong.

--- src/shinyreporter/Blocks.py
+++ src/shinyreporter/Blocks.py
@@ -2,48 +2,49 @@ from typing import List, Optional, Any, Dict
 
 __all__ = ["ContentBlock"]
 
+
 class ContentBlock:
     """
     A class representing a content block with various methods to set and retrieve content.
     """
-    
+
     def __init__(self):
         """
         Initializes an empty content block.
         """
         self._content: List[Any] = []
-        
+
     def set_content(self, content: Optional[Any]) -> None:
         """
         Sets the content of the content block.
-        
+
         Args:
             content (Optional[Any]): The content to be set. Can be of any type or None.
         """
         raise NotImplementedError("This method is not implemented.")
-        
+
     def get_content(self) -> str:
         """
         Retrieves the content of the content block.
-        
+
         Returns:
             str: The content as a string.
         """
         raise NotImplementedError("This method is not implemented.")
-        
+
     def from_list(self, x: Dict[str, Any]) -> None:
         """
         Sets the content of the content block from a dictionary.
-        
+
         Args:
             x (Dict[str, Any]): A dictionary with content data.
         """
         raise NotImplementedError("This method is not implemented.")
-        
+
     def to_list(self) -> Dict[str, Any]:
         """
         Converts the content block to a dictionary.
-        
+
         Returns:
             Dict[str, Any]: A dictionary representation of the content block.
         """
