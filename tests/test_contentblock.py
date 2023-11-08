eaea832c08f1b892bbcd5dff077f6bcba6c965be
Konrad Pagacz <konrad.pagacz@gmail.com>
2023-11-08 09:16:53 +0100

    refactor: added py.typed (#5)
    * If we want mypy and other modules to detect that the library uses typing, 
       we should add an empty file - `py.typed` - to the project.
    * This is according to [PEP-561](https://peps.python.org/pep-0561/#packaging-type-information).

--- tests/test_contentblock.py
+++ tests/test_contentblock.py
@@ -1,21 +1,25 @@
 import pytest
 from shinyreporter.Blocks import ContentBlock
 
+
 def test_set_content():
     block = ContentBlock()
     with pytest.raises(NotImplementedError, match="This method is not implemented."):
         block.set_content("Hello, World!")
+
 
 def test_get_content():
     block = ContentBlock()
     with pytest.raises(NotImplementedError, match="This method is not implemented."):
         block.get_content()
 
+
 def test_from_list():
     block = ContentBlock()
     data = {"text": "Hello, World!"}
     with pytest.raises(NotImplementedError, match="This method is not implemented."):
         block.from_list(data)
+
 
 def test_to_list():
     block = ContentBlock()
